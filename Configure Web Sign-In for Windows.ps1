805cbef16d85b2f2406683efced4f3f40c628de7
Chris Dewey <142454021+chris-dewey-1991@users.noreply.github.com>
2026-02-15 12:55:14 +0000

    Add script to configure Web Sign-In for Windows
    This script enables Web Sign-in on Windows, allowing users to sign in with TAP or Office 365 details. It includes parameters for enabling or disabling the feature and updates the registry accordingly.

---
+++ Configure Web Sign-In for Windows.ps1
@@ -0,0 +1,41 @@
+<#
+    .NOTES
+    ===========================================================================
+    Name:           Configure Web Sign-In for Windows
+    Purpose:        Use with immybot
+    Created by:     Chris Dewey
+    Updated:        2026.02.15
+    Version:        1.0
+    Changes in v1.0:
+      - Initial release
+    ===========================================================================
+    .DESCRIPTION
+    This will enable Web Sign-in on Windows to allow users to sign in with TAP or there Office 365 details.
+    This is used for our Technical Setup as we do not keep clients passwords. This allows us to utilize the TAP
+    to onboard the device to intunes and configure the device for the user.
+#>
+
+# Param are used within immybot as a selection on a form
+    param(
+    [Parameter(
+        Position=0,
+        Mandatory=$false,
+        HelpMessage=@'
+Choose whether Web Sign-in should be Disabled or Enabled.
+'@
+    )]
+    [ValidateSet('Yes','No')]
+    [string]$EnableWebSignIn = 'Yes'
+)
+
+# Map friendly choice to the registry DWORD value
+$dwordValue = switch ($EnableWebSignIn) {
+    'No' { 0 }
+    'Yes'  { 1 }
+}
+
+Get-WindowsRegistryValue `
+    -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Authentication" `
+    -Name "EnableWebSignIn" `
+    -AllowedType DWord |
+    RegistryShould-Be -Value $dwordValue
